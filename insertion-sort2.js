288a7e198e45e92ee18b05dd33b9dc67b33dfc7d
Colin McDonald <mickydwow@gmail.com>
2016-06-21 20:10:43 -0700

    Insertion sort

---
+++ insertion-sort2.js
@@ -0,0 +1,20 @@
+var fs = require('fs')
+
+function processData(input) {
+  if (!Array.isArray(input)) {
+    input = input.split('\n');
+    var length = input.splice(0, 1);
+    input = input[0].split(' ');
+  } 
+
+  for (var i = 0; i < input.length; i++) {
+    var temp = input[i]
+    for (var j = i - 1; j >= 0 && (parseInt(input[j]) > parseInt(temp)); j--) {
+      input[j+1] = input[j]
+    }
+    input[j+1] = temp;
+  console.log(input.join(' '));
+  }
+}
+
+processData(fs.readFileSync('./insertion-sort2.txt', 'utf8'))
